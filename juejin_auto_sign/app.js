92c60bbb546ae4348397ba2aa56b58808e1b294f
liming <liming@dfwsgroup.com>
2022-01-13 09:43:07 +0800

    feat✨:  优化掘金自动签到

--- juejin_auto_sign/app.js
+++ juejin_auto_sign/app.js
@@ -2,29 +2,39 @@ const got = require('got')
 
 const { cookie, aid, uuid, _signature, PUSH_PLUS_TOKEN } = require('./config')
 
-const BASEURL = 'https://api.juejin.cn/growth_api/v1/check_in' // 掘金签到api
+const urlApi = [
+  'https://api.juejin.cn/growth_api/v1/check_in',   // 掘金签到api
+  'https://api.juejin.cn/growth_api/v1/lottery_lucky/dip_lucky',  //沾幸运次数
+  'https://api.juejin.cn/growth_api/v1/lottery/draw'  //免费抽一次
+]
 const PUSH_URL = 'http://www.pushplus.plus/send' // pushplus 推送api
 
-const URL = `${BASEURL}?aid=${aid}&uuid=${uuid}&_signature=${_signature}`
 
 const HEADERS = {
   cookie,
   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67'
 }
 
-// 签到
-async function signIn () {
-  const res = await got.post(URL, {
+async function dbFuc() {
+  let reqApi = urlApi.map(v=> `${v}?aid=${aid}&uuid=${uuid}&_signature=${_signature}`)
+  let promises = reqApi.map((url) => got.post(url, {
     hooks: {
       beforeRequest: [
         options => {
           Object.assign(options.headers, HEADERS)
         }
       ]
+    },
+    json: {
+      lottery_history_id: "7005733067977719816"
     }
-  })
-  console.log(res.body)
-  handlePush(res.body)
+  }));
+  let results = [];
+  for (let promise of promises) {
+    let res = await promise
+    results.push(res.body);
+  }
+  handlePush(results)
 }
 
 // push
@@ -37,8 +47,7 @@ async function handlePush (desp) {
   const res = await got.post(PUSH_URL, {
     json: body
   })
-  console.log(res.body)
 }
 
-signIn()
+dbFuc()
 
